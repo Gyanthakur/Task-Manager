41c8cf70af8ff64f4c7b348c11db8dcd42d44495
Gyanthakur <gps.96169@gmail.com>
2025-02-10 00:35:56 +0530

    feat: added backend

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,9 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.get("/", (req, res) => {
+  res.send("API is working fine 🚀");
+});
 
 // error handler middleware
 app.use(errorHandler);
